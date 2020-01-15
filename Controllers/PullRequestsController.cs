0f07a4aaac9a68a3259bf1613862f96c285ece44
Иван Варнавский <ivan.varnavskiy@shtormtech.ru>
2020-01-15 23:44:47 +0300

    initial commit

---
+++ Controllers/PullRequestsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks.Payloads;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureDevOps.PullRequestCheckService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PullRequestsController : ControllerBase
+    {
+        private IConfiguration _config;
+        public PullRequestsController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpPost("CheckCodeCoverage")]
+        public async Task<ActionResult<string>> CheckCodeCoverage([FromBody] GitPullRequestUpdatedPayload data)
+        {
+            string pat = _config.GetSection("DevOpsServer").GetValue<string>("AccessToken");
+            string collectionUri = $"{_config.GetSection("DevOpsServer").GetValue<string>("url")}/{_config.GetSection("DevOpsServer").GetValue<string>("Collection")}";
+            var repoId = data.Resource.Repository.Id;
+            var pullRequestId = data.Resource.PullRequestId;
+            var projectId = data.Resource.Repository.Project.Id;
+
+            if (data.Resource.Status.Equals("active", StringComparison.OrdinalIgnoreCase))
+                Checkers.CodeCoverage.CheckPrCodeCoverageForBuild(collectionUri, pat, projectId, repoId, pullRequestId);
+
+            return Ok("success");
+        }
+
+        [HttpPost("CheckAuthorReview")]
+        public async Task<ActionResult<string>> CheckAuthorReview([FromBody] GitPullRequestUpdatedPayload data)
+        {
+            string pat = _config.GetSection("DevOpsServer").GetValue<string>("AccessToken");
+            string collectionUri = $"{_config.GetSection("DevOpsServer").GetValue<string>("url")}/{_config.GetSection("DevOpsServer").GetValue<string>("Collection")}";
+            var repoId = data.Resource.Repository.Id;
+            var pullRequestId = data.Resource.PullRequestId;
+            var projectId = data.Resource.Repository.Project.Id;
+
+            if (data.Resource.Status.Equals("active", StringComparison.OrdinalIgnoreCase))
+                Checkers.AuthorReview.CheckAuthorReview(collectionUri, pat, projectId, repoId, pullRequestId);
+
+            return Ok("success");
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,26 @@
+﻿# REDHAT Docker file
+FROM {dockerRegistry}/dotnet/dotnet-31-rhel7 AS build-env
+WORKDIR /app
+
+# switch to root
+USER 0
+RUN mkdir obj
+
+# Copy csproj and restore as distinct layers
+COPY . ./
+
+# REDHAT specific - we need to enable dotnet CLI
+RUN scl enable rh-dotnet31 -- dotnet restore
+
+# run unit tets - build will fail if tests fail
+# RUN scl enable rh-dotnet31 -- dotnet test Tests
+
+# build
+RUN scl enable rh-dotnet31 -- dotnet publish -c Release -o /app/out
+
+# Build runtime image
+FROM {dockerRegistry}/dotnet/dotnet-31-runtime-rhel7
+
+WORKDIR /app
+COPY --from=build-env /app/out .
+ENTRYPOINT ["sh","-c","scl enable rh-dotnet31 -- dotnet AzureDevOps.PullRequestCheckService.dll"]
